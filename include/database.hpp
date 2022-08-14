7884fdf8e5edc7c6510d6a353823a2ecf43c6e62
JCM <jcm@jcm.re>
2022-08-14 14:08:36 +0000

    Basic database functions

--- include/database.hpp
+++ include/database.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <dpp/snowflake.h>
 #include <pqxx/pqxx>
+#include <functional>
 
 namespace chocobot {
 
@@ -12,7 +14,39 @@ namespace chocobot {
 
             void prepare();
 
+            void create_user(dpp::snowflake user, dpp::snowflake guild, pqxx::work* tx = nullptr);
+            int get_coins(dpp::snowflake user, dpp::snowflake guild, pqxx::work* tx = nullptr);
+            void change_coins(dpp::snowflake user, dpp::snowflake guild, int amount, pqxx::work* tx = nullptr);
+            void set_coins(dpp::snowflake user, dpp::snowflake guild, int coins, pqxx::work* tx = nullptr);
+
             pqxx::connection m_connection;
+        protected:
+            template<typename T>
+            T tx_helper(pqxx::work* tx, std::function<T(pqxx::work& tx)> consumer)
+            {
+                bool tx_given = tx;
+                if(!tx_given) tx = new pqxx::work{m_connection};
+                T t = consumer(*tx);
+                if(!tx_given)
+                {
+                    tx->commit();
+                    delete tx;
+                }
+                return t;
+            }
+
+            template<>
+            void tx_helper(pqxx::work* tx, std::function<void(pqxx::work& tx)> consumer)
+            {
+                bool tx_given = tx;
+                if(!tx_given) tx = new pqxx::work{m_connection};
+                consumer(*tx);
+                if(!tx_given)
+                {
+                    tx->commit();
+                    delete tx;
+                }
+            }
     };
 
 }
--- src/database.cpp
+++ src/database.cpp
@@ -1,14 +1,46 @@
 #include "database.hpp"
 
+#include <spdlog/spdlog.h>
+
 namespace chocobot {
 
 void database::prepare()
 {
+    m_connection.prepare("create_user", "INSERT INTO coins(uid, guild, coins, last_daily, daily_streak) VALUES ($1, $2, 0, 0, 0)");
     m_connection.prepare("get_coins", "SELECT coins FROM coins WHERE uid=$1 AND guild=$2");
     m_connection.prepare("change_coins", "UPDATE coins SET coins=coins+$3 WHERE uid=$1 AND guild=$2");
     m_connection.prepare("set_coins", "UPDATE coins SET coins=$3 WHERE uid=$1 AND guild=$2");
     m_connection.prepare("get_stat", "SELECT value FROM user_stats WHERE uid=$1 AND guild=$2 AND stat=$3");
     m_connection.prepare("set_stat", "INSERT INTO user_stats(uid, guild, stat, value) VALUES($1, $2, $3, $4) ON CONFLICT (uid, guild, stat) DO UPDATE SET value=$4");
+}
+
+void database::create_user(dpp::snowflake user, dpp::snowflake guild, pqxx::work* tx)
+{
+    tx_helper<void>(tx, [user, guild](pqxx::work& tx){
+        tx.exec_prepared0("create_user", user, guild);
+    });
+    spdlog::debug("Created new user entry for {} in {}.", user, guild);
+}
+
+int database::get_coins(dpp::snowflake user, dpp::snowflake guild, pqxx::work* tx)
+{
+    return tx_helper<int>(tx, [user, guild](pqxx::work& tx){
+        return tx.exec_prepared1("get_coins", user, guild).at("coins").as<int>();
+    });
+}
+
+void database::change_coins(dpp::snowflake user, dpp::snowflake guild, int amount, pqxx::work* tx)
+{
+    tx_helper<void>(tx, [user, guild, amount](pqxx::work& tx){
+        tx.exec_prepared0("change_coins", user, guild, amount);
+    });
+}
+
+void database::set_coins(dpp::snowflake user, dpp::snowflake guild, int coins, pqxx::work* tx)
+{
+    tx_helper<void>(tx, [user, guild, coins](pqxx::work& tx){
+        tx.exec_prepared0("set_coins", user, guild, coins);
+    });
 }
 
 }

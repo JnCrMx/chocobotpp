2b84f23084a5ed62dc109529f58e5f5ea12c8eed
JCM <jcm@jcm.re>
2022-08-14 14:08:17 +0000

    Convert to spaces (more money!)

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@
 
 int main(int argc, const char** argv)
 {
-	if(argc != 2)
-	{
-		std::cerr << "Usage: " << argv[0] << " [config file]" << std::endl;
-		return 2;
-	}
+    if(argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " [config file]" << std::endl;
+        return 2;
+    }
 
-	spdlog::info("Starting ChocoBot...");
+    spdlog::info("Starting ChocoBot...");
 
-	nlohmann::json j;
-	{
-		std::ifstream ifconfig(argv[1]);
-		ifconfig >> j;
-	}
+    nlohmann::json j;
+    {
+        std::ifstream ifconfig(argv[1]);
+        ifconfig >> j;
+    }
 
-	chocobot::config config(j);
-	chocobot::chocobot bot(config);
-	bot.start();
+    chocobot::config config(j);
+    chocobot::chocobot bot(config);
+    bot.start();
 
-	return 0;
+    return 0;
 }

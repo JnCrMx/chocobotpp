25f300dbe15cff301bcbae079e2f5eb4b2b46907
JCM <jacama2009@gmail.com>
2022-08-15 03:36:01 +0200

    Log level from env

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <spdlog/spdlog.h>
+#include <spdlog/cfg/env.h>
 
 #include "config.hpp"
 #include "chocobot.hpp"
 
 int main(int argc, const char** argv)
 {
+    spdlog::cfg::load_env_levels();
     if(argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << " [config file]" << std::endl;

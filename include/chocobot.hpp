b6a65fea98931ab550705daf67a37fb3f2c10902
JCM <jcm@jcm.re>
2022-09-01 10:28:33 +0000

    Reduce request_threads

--- include/chocobot.hpp
+++ include/chocobot.hpp
@@ -12,7 +12,12 @@ namespace chocobot {
 class chocobot
 {
     public:
-        chocobot(config config) : m_config(config), m_db(config.db_uri), m_bot(config.token, dpp::i_default_intents | dpp::i_message_content) {
+        chocobot(config config) : m_config(config), m_db(config.db_uri), 
+            m_bot(config.token, dpp::i_default_intents | dpp::i_message_content, /*token, intents*/
+                  0, 0, 1, true,  /*shards, cluster_id, maxclusters, compressed*/
+                  {dpp::cp_aggressive, dpp::cp_none, dpp::cp_none}, /*policy*/
+                  4, 1) /*request_threads, request_threads_raw*/
+        {
             init();
         }
 
